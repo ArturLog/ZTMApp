65b2695a2105370cf3d15e9530cb0e37b4f2fda7
ArturLog <artur.spiewak2967@gmail.com>
2025-01-07 13:19:47 +0100

    Configure login with jwt

--- ztmapp/src/auth/auth.module.ts
+++ ztmapp/src/auth/auth.module.ts
@@ -3,16 +3,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: {
-        expiresIn: '3600000'
-      }
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get("JWT_SECRET"),
+        signOptions: {
+          expiresIn: "3600000",
+        },
+      }),
     })
   ],
   controllers: [AuthController],
